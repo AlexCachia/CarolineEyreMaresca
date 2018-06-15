d0bd1cdd5edbcc364091a1f5657ac030eb20c038
AlexCachia <40211094+AlexCachia@users.noreply.github.com>
2018-06-15 12:49:01 +0100

    Add files via upload

---
+++ includes/shoutOutWrapper.php
@@ -0,0 +1,26 @@
+<div id = "shoutOutWrapper">
+	<div id = "locationDiv">
+		<p class="shoutOutTitle"><strong itemprop="description">Experienced Vocal Coach and singer based in Maidstone, Kent</strong></p>
+	</div>
+	<div id = "availableDiv">
+		<p class="shoutOutTitle" >Available for...</p>
+		<span id="soloSpan">Solo</span><br/>
+		<span id="duoSpan">Duo</span><br/>
+		<span id="bandSpan">Band</span><br/>
+		<span id="recordingSpan">Recording</span><br/>
+		<span id="teachingSpan">Teaching</span>
+        <span id="weddingSpan">Weddings</span>
+	</div>
+	<div id = "linkDiv">
+		<p class="shoutOutTitle">Also find me on...</p>
+		<a href="https://www.facebook.com/CarolineEyreMaresca" onclick="window.open(this.href,'_blank'); return false;" title="Visit Caroline Eyre Maresca Facebook Page">
+			<img class= "shoutOutImage" src="media/images/facebook.png" alt="Visit Facebook Page" >
+		</a>  <br/>
+		<a style="display: none;" href="https://twitter.com/112233Cazzi" onclick="window.open(this.href,'_blank'); return false;" title="Visit Caroline Eyre Maresca Twitter Page">
+			<img class= "shoutOutImage" src="media/images/twitter.png" alt="Visit Twitter Page" >
+		</a>
+		<a href="https://uk.linkedin.com/in/caroline-maresca-39802973" onclick="window.open(this.href,'_blank'); return false;" title="Visit Caroline Eyre Maresca LinkedIn Page">
+			<img  src="media/images/linkedin.png" alt="Visit LinkedIn Page" title="Visit LinkedIn Page">
+		</a>
+</div>
+</div>
