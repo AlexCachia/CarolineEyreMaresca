ff0b3ce397385eaf6b6fd20abbb2f5ec9015a004
AlexCachia <40211094+AlexCachia@users.noreply.github.com>
2018-06-15 13:57:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,27 +2,16 @@
 	$jsArray=array();
 	include 'includes/header.php';
 ?>
-	<h2>Caroline Eyre Maresca  BA (QTS)</h2>
+	<h2>Title</h2>
 	<span itemprop="image">
-		<img id = "bioImage" class = "mainImage" alt ="Maidstone, Kent based vocal coach and singer" title="Caroline Eyre Maresca is a Maidstone based vocal coach and singer" src="media/images/bio.jpg"/>
+		<img id = "bioImage" class = "mainImage" alt ="Bio Image" title="Bio Image" src="media/images/bio.jpg"/>
 	</span>
-	<p>
-		As a professional singer and also a qualified teacher, I am very fortunate to have a career doing both the things I love in equal quantities.
-	</p>	
-	<p>
-		As a singer, I perform in venues ranging from the most intimate of restaurants to the largest theatres in Europe, using both my ability to front a show and my skills with harmony singing as a backing vocalist. I also perform for a range of events and functions such as weddings.
-	</p>
-	<p>
-		As a qualified teacher, I am trained to teach singing and breathing techniques to both children and adults. I also offer a tuition service supporting children who are struggling with reading and writing.
-	</p>
-	<p>
-		For both singing and teaching, most of my work is now mainly based around London and the South East but I am more than happy to travel further when required. Just indicate the location for the your function on my contact page.
-	</p>
+	<p>Placeholder Text</p>	
 	<p id = "linkPara">
-		<a href="https://www.facebook.com/CarolineEyreMaresca" onclick="window.open(this.href,'_blank'); return false;" title="Visit Caz Eyre Maresca Facebook Page">
+		<a href="https://www.facebook.com/FacebookLink" onclick="window.open(this.href,'_blank'); return false;" title="Visit Facebook Page">
 			<img class = "linkImage" src="media/images/facebook.png" alt="Visit Facebook Page" >
 		</a>  
-		<a href="https://uk.linkedin.com/in/caroline-maresca-39802973" onclick="window.open(this.href,'_blank'); return false;" title="Visit Caz Eyre Maresca LinkedIn Page">
+		<a href="https://uk.linkedin.com/in/LinkedInLink" onclick="window.open(this.href,'_blank'); return false;" title="Visit LinkedIn Page">
 			<img class = "linkImage" src="media/images/linkedin.png" alt="Visit LinkedIn Page" >
 		</a>
 	</p>
@@ -33,7 +22,6 @@
 include 'includes/shoutOutWrapper.php';
 ?>
 </div>
-<meta itemprop="url" content="http://www.caroline-eyre-maresca.com/">
 </div>
 </body>
 </html>
