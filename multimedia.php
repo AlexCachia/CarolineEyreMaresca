540cc084e2ee4e08d4000e7e353f09fea8b2d024
AlexCachia <40211094+AlexCachia@users.noreply.github.com>
2018-06-22 12:05:10 +0100

    Update multimedia.php

--- multimedia.php
+++ multimedia.php
@@ -5,7 +5,7 @@
 
 <h2>Multimedia</h2>
 
-<iframe id="scIFRAME" height="350"  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/279632043&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+<iframe id="scIFRAME" height="350"  src="https://w.soundcloud.com/player/?url=example"></iframe>
 
 </div>
 
